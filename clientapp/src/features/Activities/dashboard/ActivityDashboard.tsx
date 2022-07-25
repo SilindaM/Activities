0953913c38a866a0d3f55bff49c798b56c22f26d
MSilinda <silindanemdu@gmail.com>
2022-07-25 15:59:09 +0200

    activity details

--- clientapp/src/features/Activities/dashboard/ActivityDashboard.tsx
+++ clientapp/src/features/Activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Grid, List } from "semantic-ui-react";
 import { Activity } from "../../../Models/activity";
+import ActivityDetails from "../details/ActivityDetails";
 import ActivityList from "./ActivityList";
 
 interface Props{
@@ -9,8 +10,16 @@ interface Props{
 
 export default function ActivityDashboard({activities}:Props){
     return(
+        <Grid>
         <Grid.Column width='10'>
             <ActivityList activities={activities}/>
         </Grid.Column>
+        <Grid.Column width='10'>
+            {activities[0]&&
+            <ActivityDetails activity={activities[0]}/>}
+        </Grid.Column>
+        
+        </Grid>
+       
     )
 }
