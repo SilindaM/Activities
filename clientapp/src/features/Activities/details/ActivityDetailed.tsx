c64b60b2b8df1f16209d303a233e8404f7f64217
MSilinda <silindanemdu@gmail.com>
2022-08-03 09:00:54 +0200

    Create components for activity lists

---
+++ clientapp/src/features/Activities/details/ActivityDetailed.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ActivityDetailed(){
+    return(
+        <h1>Detailed</h1>
+    )
+}
---
+++ clientapp/src/features/Activities/details/ActivityDetailedSideBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ActivityDetailedSideBar(){
+    return(
+        <h1>Sidebar</h1>
+    )
+}
