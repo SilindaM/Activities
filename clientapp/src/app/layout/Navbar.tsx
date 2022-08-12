ac04f13f186903aad450adff5ebad805fbfa73b0
SilindaM <silindanemdu@gmail.com>
2022-08-12 01:57:56 -0700

    Add dropdown in the navbar for login in

--- clientapp/src/app/layout/Navbar.tsx
+++ clientapp/src/app/layout/Navbar.tsx
@@ -1,14 +1,16 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
-import { NavLink } from "react-router-dom";
-import { Button, Container, Menu } from "semantic-ui-react";
+import { Link, NavLink } from "react-router-dom";
+import { Button, Container, Menu,Image, Dropdown } from "semantic-ui-react";
 import { useStore } from "../stores/store";
 
 interface Props{
     openForm: () => void;
 }
 
-export default function NavBar(){
+export default observer( function NavBar(){
     const {activityStore} = useStore();
+    const {userStore:{user,logout}}=useStore();
     return(
         <Menu inverted fixed='top'>
             <Container>
@@ -21,7 +23,16 @@ export default function NavBar(){
                 <Menu.Item as={NavLink} to='/createActivity'>
                     <Button  positive content='Create Activity'/>
                 </Menu.Item>
+                <Menu.Item position="right">
+                    <Image src={user?.image || '/assets/user.png'} avatar  spaced='right'/>
+                    <Dropdown pointing='top left' text={user?.displayName}>
+                        <Dropdown.Menu>
+                            <Dropdown.Item as={Link} to={`/profile/${user?.username}`} text='My Profile' icon='user'/>
+                            <Dropdown.Item  onClick={logout}  text='Logout' icon='power'/>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </Menu.Item>
             </Container>
         </Menu>
     )
-}
+})
--- clientapp/src/features/home/HomePage.tsx
+++ clientapp/src/features/home/HomePage.tsx
@@ -1,8 +1,11 @@
+import { observer } from "mobx-react-lite";
 import React  from "react";
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Segment,Image } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
 
-export default function HomePage(){
+export default observer( function HomePage(){
+    const {userStore}=useStore();
     return(
             <Segment inverted textAlign="center" vertical className="masthead">
                 <Container text>
@@ -10,11 +13,21 @@ export default function HomePage(){
                         <Image size='massive' src='/assets/logo.png'  alt='logo' style={{marginBottom:12}}/>
                         React
                     </Header>
-                    <Header as='h2' inverted content='Welcome To Activities'/>
-                    <Button as={Link} to='/login' size='huge' inverted>
-                        Login
-                    </Button>
+                        {userStore.isLoggedIn?(
+                            <>
+                                 <Header as='h2' inverted content='Welcome To Activities'/> 
+                                 <Button as={Link} to='/activities' size='huge' inverted>
+                                      Go to Activities
+                                 </Button>
+                            </>
+                        ):
+                        (
+                                 <Button as={Link} to='/login' size='huge' inverted>
+                                      Login!
+                                 </Button>
+                        )}
+                   
                 </Container>
             </Segment>
     )
-}
+})
