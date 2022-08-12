a883f7ce8ebfc4e224941d544e33b5e212ecf301
SilindaM <silindanemdu@gmail.com>
2022-08-12 01:43:36 -0700

    user login into the react app

--- clientapp/src/app/stores/commonStore.ts
+++ clientapp/src/app/stores/commonStore.ts
@@ -3,11 +3,19 @@ import { ServerError } from "../../Models/serverError";
 
 export default class commonStore{
     error:ServerError |null =null;
+    token:string|null=null;
+    appLoaded=false;
 
  constructor(){
     makeAutoObservable(this)
  }
  setServerError=(error:ServerError)=>{
     this.error=error;
+ }
+ setToken=(token:string|null)=>{
+   if(token) window.localStorage.setItem('jwt',token);
+ }
+ setAppLoaded=()=>{
+this.appLoaded=true
  }
 }
--- clientapp/src/app/stores/userStore.ts
+++ clientapp/src/app/stores/userStore.ts
@@ -1,6 +1,8 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, runInAction } from "mobx";
+import { history } from "../..";
 import { User, UserFormValues } from "../../Models/user";
 import agent from "../api/agent";
+import { store } from "./store";
 
 export default class UserStore{
     user:User|null=null;
@@ -14,9 +16,20 @@ export default class UserStore{
     login=async(creds:UserFormValues)=>{
         try {
             const user=await agent.Account.login(creds);
+            store.commonStore.setToken(user.token);
+            runInAction(()=>{
+                this.user=user;
+                history.push('/activities');
+            })
             console.log(user);
         } catch (error) {
             throw error;
         }
+    }
+    logout=()=>{
+        store.commonStore.setToken(null);
+        window.localStorage.removeItem('jwt');
+        this.user=null;
+        history.push('/');
     }
 }
